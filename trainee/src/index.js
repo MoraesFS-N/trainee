b26b4ed8b3b7cbb89e86d96aa9ab758b8f769d18
Moraes <moraesfelicioneto@hotmail.com>
2021-01-06 19:18:25 -0300

    Fundamentos de React

---
+++ trainee/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import Primeiro from './components/Primeiro';
+import ComParametro from './components/ComParametro';
+
+ReactDOM.render(
+    <Primeiro />,
+    <ComParametro />,
+    document.getElementById('root')
+)
